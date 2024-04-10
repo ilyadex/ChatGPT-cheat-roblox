16a45329c2d69173210c4c5c3b9926d319409855
ilyadex <108224384+ilyadex@users.noreply.github.com>
2024-04-10 21:27:14 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheScriptMaster1/ScriptMaster-Hub/main/scriptmasterhub.lua')))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/ilyadex/ChatGPT-cheat-roblox/main/esp.lua')))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/ilyadex/ChatGPT-cheat-roblox/main/radar.lua')))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TheScriptMaster1/ScriptMaster-Hub/main/scriptmasterhub.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ilyadex/ChatGPT-cheat-roblox/main/esp.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ilyadex/ChatGPT-cheat-roblox/main/radar.lua'))()
